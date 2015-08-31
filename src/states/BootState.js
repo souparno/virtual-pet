3423c7a8a9719c805853cb1b5cf1ffdac97400ce
Uldzh <uldis.megnis@gmail.com>
2015-08-31 06:33:12 +0300

    Boot state

--- src/states/BootState.js
+++ src/states/BootState.js
@@ -1,12 +1,25 @@
 class BootState extends Phaser.State
 {
+    construcotr(props)
+    {
+        super(props);
+
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+    }
 
     preload()
     {
+        this.load.image('preloadBar', 'images/bar.png');
+        this.load.image('logo', 'images/logo.png');
     }
 
 	create()
     {
+        this.game.stage.backgroundColor = '#fff';
+        // Start preload state
+        this.state.start('PreloadState');
 	}
 
 }
